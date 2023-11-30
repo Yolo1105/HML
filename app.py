9b46f71ebae6fd5ebd404679fac1c2f5cb6c46cc
hongjie4531 <hc2981@nyu.edu>
2023-11-29 22:19:56 -0500

    test

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template
 import pandas as pd
 from flask_mysqldb import MySQL
 
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DATABSE'] = 'jail'
+app.config['MYSQL_HOST'] = "localhost"
+app.config['MYSQL_USER'] = "root"
+app.config['MYSQL_PASSWORD'] = ""
+app.config['MYSQL_DB'] = "jail"
+
 
 mysql = MySQL(app)
 
@@ -19,20 +20,20 @@ def runstatement(statement):
     #convert into a panda dataframe
     df = ""
     if (cursor.description):
-        column_names = [col[0] for desc in cursor.description]
+        column_names = [desc[0] for desc in cursor.description]
         df = pd.DataFrame(results, columns=column_names)
     cursor.close()
     return df
 
-
-
 @app.route('/')
 def index():
+    example = runstatement("SELECT * FROM officers")
+    #print(example)
     return render_template('index.html')
 
 @app.route('/test')
 def test():
-    return render_template('test.html',)
+    return render_template('test.html')
 
 
 if __name__ == '__main__':
